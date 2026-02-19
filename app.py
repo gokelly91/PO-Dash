aeae34522f7dbf8f30ac9b5fb83ed8db1a943be9
gokelly91 <gerald@mdtools.co.za>
2026-02-19 07:55:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -175,6 +175,9 @@ def get_open_pos():
 # ===============================
 # RUN
 # ===============================
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
+
 
